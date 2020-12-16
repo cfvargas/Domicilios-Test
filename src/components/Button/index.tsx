ee5335af9798c2a482769ff66c7b704005be2997
Cristian Vargas <cristianv975@gmail.com>
2020-12-16 18:44:50 -0500

    #add: Button componente

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,24 @@
+import {ButtonHTMLAttributes} from 'react'
+import styled from 'styled-components'
+import appearances from './appearences'
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  appearance: 'primary' | 'secondary'
+}
+
+const Button = styled.button<ButtonProps>`
+  border: none;
+  padding: 7px 1.5rem;
+  border-radius: 3px;
+  outline: none;
+  cursor: pointer;
+
+  color: ${props => appearances[props.appearance].text};
+  background-color: ${props => appearances[props.appearance].background};
+
+  &:active {
+    transform: scale(0.95);
+  }
+`
+
+export default Button
