3b7a2ad68b3bdb7d083ca78065243f7b082fb337
Cristian Vargas <cristianv975@gmail.com>
2020-12-16 22:30:50 -0500

    #add: css to post component

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ const fakePost: postType = {
   user: {
     firstName: 'Cristian',
     lastName: 'Vargas',
-    picture: 'https://cristian-picture.png',
+    picture:
+      'https://scontent.fbog4-1.fna.fbcdn.net/v/t1.0-9/129953134_10221344432593788_6349793943326329587_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=4iJkllzunwUAX9EPuPW&_nc_ht=scontent.fbog4-1.fna&oh=c520abba32a59eb63a3d036be33ea0a1&oe=6000C1C1',
   },
-  content: 'Mi primer publicación',
+  content:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique totam magnam architecto vero, tenetur illum consequatur provident non, assumenda earum cum? Ab fugiat voluptatem itaque illum, numquam similique quos earum?',
   created: new Date(),
 }
 
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import moment from 'moment'
+import styled from 'styled-components'
+import colors from '../../config/colors'
+import {Box} from '../'
 
 export type userType = {
   firstName: string
@@ -18,12 +21,67 @@ export interface PostProps {
 }
 
 const Post = ({post}: PostProps) => (
-  <article>
-    <p>{`${post.user.firstName} ${post.user.lastName}`}</p>
-    <p>{moment(post.created).fromNow()}</p>
-    <p>{post.content}</p>
-    <img alt={`${post.user.firstName}'s avatar`} src={post.user.picture} />
-  </article>
+  <Post.Element>
+    <Box>
+      <Box.Body>
+        <Post.Body>
+          <Post.Avatar>
+            <img
+              alt={`${post.user.firstName}'s avatar`}
+              src={post.user.picture}
+            />
+          </Post.Avatar>
+          <Post.Description>
+            <div>
+              <Post.UserName>{`${post.user.firstName} ${post.user.lastName}`}</Post.UserName>
+              <Post.Created>{moment(post.created).fromNow()}</Post.Created>
+            </div>
+            <Post.Content>{post.content}</Post.Content>
+          </Post.Description>
+        </Post.Body>
+      </Box.Body>
+    </Box>
+  </Post.Element>
 )
+
+Post.Element = styled.article``
+Post.Body = styled.div`
+  display: flex;
+  align-items: flex-start;
+`
+Post.Avatar = styled.picture`
+  min-width: 50px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    max-width: 100%;
+  }
+`
+
+Post.UserName = styled.span`
+  font-weight: bold;
+  color: ${colors.green};
+`
+
+Post.Created = styled.span`
+  color: ${colors.gray};
+  font-size: 13px;
+  display: block;
+`
+
+Post.Content = styled.p`
+  margin: 10px 0;
+  font-size: 15px;
+  line-height: 1.5;
+`
+
+Post.Description = styled.div`
+  width: 100%;
+  padding: 0 1rem;
+`
 
 export default Post
